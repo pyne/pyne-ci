b2b6b73d58390ee606077d9ebe03bf9f267add8a
Matthew Gidden <matthew.gidden@gmail.com>
2014-03-06 16:37:12 -0600

    reverted to full stack

--- build.sh
+++ build.sh
@@ -8,33 +8,33 @@ echo $PATH
 export PYTHONPATH=`pwd`/install/lib/python2.7/site-packages:$PYTHONPATH:`pwd`/install
 export DYLD_FALLBACK_LIBRARY_PATH=`pwd`/../install/lib/python2.7/site-packages/pyne/lib:$DYLD_FALLBACK_LIBRARY_PATH
 
-# cd hdf5-1.8.4
-# ./configure --prefix=`pwd`/../install --enable-shared 
-# make
-# make install
-# cd ..
+cd hdf5-1.8.4
+./configure --prefix=`pwd`/../install --enable-shared 
+make
+make install
+cd ..
 
-# cd moab-4.6.2
-# ./configure --prefix=`pwd`/../install --enable-shared --with-hdf5=`pwd`/../install
-# make
-# make install
-# cd ..
+cd moab-4.6.2
+./configure --prefix=`pwd`/../install --enable-shared --with-hdf5=`pwd`/../install
+make
+make install
+cd ..
 
 cd nose
 python setup.py install --prefix=`pwd`/../install
-# cd ../numpy
-# python setup.py install --prefix=`pwd`/../install
-# cd ../cython
-# python setup.py install --prefix=`pwd`/../install
-# cd ../scipy
-# python setup.py install --prefix=`pwd`/../install
-# cd ../numexpr
-# python setup.py install --prefix=`pwd`/../install
-# cd ../PyTables
-# python setup.py install --prefix=`pwd`/../install --hdf5=`pwd`/../install
+cd ../numpy
+python setup.py install --prefix=`pwd`/../install
+cd ../cython
+python setup.py install --prefix=`pwd`/../install
+cd ../scipy
+python setup.py install --prefix=`pwd`/../install
+cd ../numexpr
+python setup.py install --prefix=`pwd`/../install
+cd ../PyTables
+python setup.py install --prefix=`pwd`/../install --hdf5=`pwd`/../install
 
 cd ..
-tar -zcvf results.tar.gz install
+tar -pczf results.tar.gz install
 
 cd pyne
 python setup.py install --prefix=`pwd`/../install --hdf5=`pwd`/../install
