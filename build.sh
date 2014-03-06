5f38bc9a1b9201084cafde98203147bd15941f39
Matthew Gidden <matthew.gidden@gmail.com>
2014-03-06 17:34:38 -0600

    updated exports

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p `pwd`/install/lib/python2.7/site-packages
 export PATH=`pwd`/install/bin:$PATH
 echo $PATH
 export PYTHONPATH=`pwd`/install/lib/python2.7/site-packages:$PYTHONPATH
-export DYLD_FALLBACK_LIBRARY_PATH=`pwd`/../install/lib/python2.7/site-packages/pyne/lib:$DYLD_FALLBACK_LIBRARY_PATH
+export DYLD_FALLBACK_LIBRARY_PATH=`pwd`/install/lib/python2.7/site-packages/pyne/lib:$DYLD_FALLBACK_LIBRARY_PATH
 export C_INCLUDE_PATH=`pwd`/install/include:$C_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=`pwd`/install/include:$CPLUS_INCLUDE_PATH
 export LIBRARY_PATH=`pwd`/install/lib:$LIBRARY_PATH
