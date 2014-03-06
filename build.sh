27d318c09cda816888fe3ca94cb0135d6e514a85
Matthew Gidden <matthew.gidden@gmail.com>
2014-03-06 16:08:24 -0600

    updated python path and reinstantiated full stack

--- build.sh
+++ build.sh
@@ -10,26 +10,26 @@ make
 make install
 cd ..
 
-# cd moab-4.6.2
-# ./configure --prefix=`pwd`/../install --enable-shared --with-hdf5=`pwd`/../install
-# make
-# make install
-# cd ..
+cd moab-4.6.2
+./configure --prefix=`pwd`/../install --enable-shared --with-hdf5=`pwd`/../install
+make
+make install
+cd ..
 
-# mkdir -p `pwd`/install/lib/python2.7/site-packages
-# export PYTHONPATH=$PYTHONPATH:`pwd`/install:`pwd`/install/lib/python2.7/site-packages
-# cd nose
-# python setup.py install --prefix=`pwd`/../install
-# cd ../numpy
-# python setup.py install --prefix=`pwd`/../install
-# cd ../cython
-# python setup.py install --prefix=`pwd`/../install
-# cd ../scipy
-# python setup.py install --prefix=`pwd`/../install
-# cd ../numexpr
-# python setup.py install --prefix=`pwd`/../install
-# cd ../PyTables
-# python setup.py install --prefix=`pwd`/../install --hdf5=`pwd`/../install
+mkdir -p `pwd`/install/lib/python2.7/site-packages
+export PYTHONPATH=`pwd`/install/lib/python2.7/site-packages:$PYTHONPATH:`pwd`/install
+cd nose
+python setup.py install --prefix=`pwd`/../install
+cd ../numpy
+python setup.py install --prefix=`pwd`/../install
+cd ../cython
+python setup.py install --prefix=`pwd`/../install
+cd ../scipy
+python setup.py install --prefix=`pwd`/../install
+cd ../numexpr
+python setup.py install --prefix=`pwd`/../install
+cd ../PyTables
+python setup.py install --prefix=`pwd`/../install --hdf5=`pwd`/../install
 
 cd ..
 tar -zcvf results.tar.gz install
