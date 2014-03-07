2797c044bb6ea97f631c31807c479e15de12fc76
Matthew Gidden <matthew.gidden@gmail.com>
2014-03-06 19:55:21 -0600

    updated python path

--- fast_build.sh
+++ fast_build.sh
@@ -20,7 +20,7 @@ fi
 
 export PATH=`pwd`/install/bin:`pwd`/install/usr/local/bin:$PATH
 echo $PATH
-export PYTHONPATH=`pwd`/install/lib/python2.7/site-packages:`pwd`/install/usr/local/lib/python2.7/site-packages:$PYTHONPATH
+export PYTHONPATH=`pwd`/install/lib/python2.7/site-packages:`pwd`/install/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages:$PYTHONPATH
 export DYLD_FALLBACK_LIBRARY_PATH=`pwd`/install/lib/python2.7/site-packages/pyne/lib:$DYLD_FALLBACK_LIBRARY_PATH
 export C_INCLUDE_PATH=`pwd`/install/include:$C_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=`pwd`/install/include:$CPLUS_INCLUDE_PATH
