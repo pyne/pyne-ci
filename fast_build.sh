084d76c1698375170bc7909243fba7bc0d0fd893
Matthew Gidden <matthew.gidden@gmail.com>
2014-03-08 16:12:52 -0600

    fast building should now work. need to wait for long build to finish to test

--- fast_build.sh
+++ fast_build.sh
@@ -16,43 +16,47 @@ elif [[ $platform == 'darwin' ]]; then
     tar -xzf mac.tar.gz
 fi
 
+PYINSTALL=`pwd`/install/lib/python2.7/site-packages
+
+mkdir -p $PYINSTALL
+
 export PATH=`pwd`/install/bin:`pwd`/install/usr/local/bin:$PATH
 echo $PATH
-export PYTHONPATH=`pwd`/install/lib/python2.7/site-packages:`pwd`/install/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages:$PYTHONPATH
+export PYTHONPATH=`pwd`/install/lib/python2.7/site-packages:`pwd`/install/usr/local/lib/python2.7/site-packages:$PYTHONPATH
 export DYLD_FALLBACK_LIBRARY_PATH=`pwd`/install/lib/python2.7/site-packages/pyne/lib:$DYLD_FALLBACK_LIBRARY_PATH
 export C_INCLUDE_PATH=`pwd`/install/include:$C_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=`pwd`/install/include:$CPLUS_INCLUDE_PATH
 export LIBRARY_PATH=`pwd`/install/lib:$LIBRARY_PATH
 export LD_LIBRARY_PATH=`pwd`/install/lib:$LD_LIBRARY_PATH
 
-python get-pip.py --root `pwd`/install
-ls -R `pwd`/install
+# install all python
+cd nose
+python setup.py install --prefix=`pwd`/../install --skip-build
+cd ..
 
-`pwd`/install/Library/Frameworks/Python.framework/Versions/2.7/bin/pip install --root `pwd`/install nose
-`pwd`/install/Library/Frameworks/Python.framework/Versions/2.7/bin/pip install --root `pwd`/install numpy
-`pwd`/install/Library/Frameworks/Python.framework/Versions/2.7/bin/pip install --root `pwd`/install cython
-`pwd`/install/Library/Frameworks/Python.framework/Versions/2.7/bin/pip install --root `pwd`/install scipy
-`pwd`/install/Library/Frameworks/Python.framework/Versions/2.7/bin/pip install --root `pwd`/install numexpr
-# cd nose
-# python setup.py install --prefix=`pwd`/../install
-# cd ../numpy
-# python setup.py install --prefix=`pwd`/../install
-# cd ../cython
-# python setup.py install --prefix=`pwd`/../install
-# cd ../scipy
-# python setup.py install --prefix=`pwd`/../install
-# cd ../numexpr
-# python setup.py install --prefix=`pwd`/../install
+cd numpy
+python setup.py install --prefix=`pwd`/../install --skip-build
+cd ..
+
+cd cython
+python setup.py install --prefix=`pwd`/../install --skip-build
+cd ..
+
+cd scipy
+python setup.py install --prefix=`pwd`/../install --skip-build
+cd ..
+
+cd numexpr
+python setup.py install --prefix=`pwd`/../install --skip-build
+cd ..
+
 cd PyTables
 python setup.py install --prefix=`pwd`/../install --hdf5=`pwd`/../install
 cd ..
-
+ 
 cd pyne
-echo "pwd and env"
-echo $PWD
-env
 python setup.py install --prefix=`pwd`/../install --hdf5=`pwd`/../install
-# -- -DCMAKE_PREFIX_PATH=`pwd`/../install
+# -- -DMOAB_INCLUDE_DIR=`pwd`/../install/include -DMOAB_LIBRARY=`pwd`/../install/lib
 
 cd scripts
 env
