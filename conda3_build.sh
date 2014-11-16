8e3d021f02705b82c0d4b7d849309bd58994e4d2
Anthony Scopatz <scopatz@gmail.com>
2014-11-16 11:42:39 -0800

    more edits

--- conda3_build.sh
+++ conda3_build.sh
@@ -1,35 +1,21 @@
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-    chmod +x Miniconda3-3.7.0-Linux-x86_64.sh
-    ./Miniconda3-3.7.0-Linux-x86_64.sh -b -p `pwd`/anaconda
-elif [[ "$unamestr" == 'Darwin' ]]; then
-    chmod +x Miniconda3-3.3.0-MacOSX-x86_64.sh
-    ./Miniconda3-3.3.0-MacOSX-x86_64.sh -b -p `pwd`/anaconda
-fi
+#!/bin/bash
+set -x
+set -e
 
-export LD_LIBRARY_PATH=`pwd`/anaconda/lib:$LD_LIBRARY_PATH
-export DYLD_FALLBACK_LIBRARY_PATH=`pwd`/anaconda/lib:$DYLD_FALLBACK_LIBRARY_PATH
-export DYLD_LIBRARY_PATH=`pwd`/anaconda/lib:$DYLD_LIBRARY_PATH
-export C_INCLUDE_PATH=`pwd`/anaconda/include:$C_INCLUDE_PATH
-export CPLUS_INCLUDE_PATH=`pwd`/anaconda/include:$CPLUS_INCLUDE_PATH
-export LIBRARY_PATH=`pwd`/anaconda/lib:$LIBRARY_PATH
-export LD_LIBRARY_PATH=`pwd`/anaconda/lib:$LD_LIBRARY_PATH
-export HDF5_ROOT=`pwd`/anaconda
+# Install conda
+export MINICONDA_PYVER="3"
+./bin/conda-inst.sh
+source conda_env.sh
 
-cdir=`pwd`/anaconda
-export PATH=`pwd`/anaconda/bin:`pwd`/anaconda/usr/local/bin:$PATH
-conda install conda-build jinja2 nose setuptools pytables hdf5 scipy cython cmake
-if [[ "$unamestr" == 'Linux' ]]; then
-    conda install patchelf
-fi
+# install deps
+conda install nose pytables hdf5 scipy cython cmake
 
+# Install PyNE
 cd pyne
-python setup.py install --prefix=`pwd`/../anaconda --hdf5=`pwd`/../anaconda
-
+python setup.py install --prefix="${CONDIR}" --hdf5="${CONDIR}" -- \
+  -DMOAB_INCLUDE_DIR="${CONDIR}/include" -DMOAB_LIBRARY="${CONDIR}/lib"
 cd scripts
 env
 nuc_data_make
-
 cd ../..
 
-exit $?
--- conda_fast_build.sh
+++ conda_fast_build.sh
@@ -5,24 +5,14 @@ set -e
 # Install conda
 export MINICONDA_PYVER="2"
 ./bin/conda-inst.sh
-
-# setup env
-condir="$(pwd)/anaconda"
-export PATH="${condir}/bin:${condir}/usr/local/bin:${PATH}"
-export LD_LIBRARY_PATH="${condir}/lib:$LD_LIBRARY_PATH"
-export DYLD_FALLBACK_LIBRARY_PATH="${condir}/lib:$DYLD_FALLBACK_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="${condir}/lib:$DYLD_LIBRARY_PATH"
-export C_INCLUDE_PATH="${condir}/include:$C_INCLUDE_PATH"
-export CPLUS_INCLUDE_PATH="${condir}/include:$CPLUS_INCLUDE_PATH"
-export LIBRARY_PATH="${condir}/lib:$LIBRARY_PATH"
-export HDF5_ROOT="${condir}"
+source conda_env.sh
 
 # install deps
 conda install nose pytables hdf5 scipy cython cmake
 
 #install MOAB
 cd moab-4.6.3
-./configure --prefix="${condir}" --enable-shared --with-hdf5="${condir}"
+./configure --prefix="${CONDIR}" --enable-shared --with-hdf5="${CONDIR}"
 make
 make install
 cd ..
@@ -30,13 +20,13 @@ cd ..
 # Install PyTAPS
 cd PyTAPS-1.4
 python setup.py build
-python setup.py install --skip-build --prefix="${condir}"
+python setup.py install --skip-build --prefix="${CONDIR}"
 cd ..
 
 # Install PyNE
 cd pyne
-python setup.py install --prefix="${condir}" --hdf5="${condir}" -- \
-  -DMOAB_INCLUDE_DIR="${condir}/include" -DMOAB_LIBRARY="${condir}/lib"
+python setup.py install --prefix="${CONDIR}" --hdf5="${CONDIR}" -- \
+  -DMOAB_INCLUDE_DIR="${CONDIR}/include" -DMOAB_LIBRARY="${CONDIR}/lib"
 cd scripts
 env
 nuc_data_make
