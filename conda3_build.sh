a6379632545a987519555007ce03d9fc1fe45ff6
Anthony Scopatz <scopatz@gmail.com>
2015-02-23 22:55:40 -0600

    try binstar config

--- conda3_build.sh
+++ conda3_build.sh
@@ -10,9 +10,7 @@ source conda_env.sh
 # install deps
 conda install nose pytables hdf5 scipy cython cmake
 #export CERT_FILE=$(ls $CONDIR/lib/python*/site-packages/requests/cacart.pem)
-conda update openssl
-find $CONDIR
-#export CERT_FILE=$CONDIR/ssl/cacart.pem
+binstar config --set ssl_options.certfile $CONDIR/ssl/cacart.pem
 conda install -c https://conda.binstar.org/cyclus lapack
 
 # Install PyNE
