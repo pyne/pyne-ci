99898f1084f62ab6688320474bec19200d66ec2f
Anthony Scopatz <scopatz@gmail.com>
2015-03-05 15:25:09 -0600

    dagmc changes needed

--- pyne/build.sh
+++ pyne/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 unset LDFLAGS
+export PATH="${PREFIX}/bin:${PATH}"
+
 ${PYTHON} setup.py install || exit 1;
+nuc_data_make
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-cp build/pyne/lib/*.dylib $PREFIX/lib
+    cp build/pyne/lib/*.dylib $PREFIX/lib
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-find . -name '*.so' | xargs chmod +x
-cp build/pyne/lib/*.so $PREFIX/lib
+    find . -name '*.so' | xargs chmod +x
+    cp build/pyne/lib/*.so $PREFIX/lib
 fi
+
