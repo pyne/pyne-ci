31e37a058bbc2daa97a6f0fdf8e1615d704a3059
Anthony Scopatz <scopatz@gmail.com>
2015-03-05 17:20:56 -0600

    pyne working locally

--- pyne/build.sh
+++ pyne/build.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
+set -e
+
 unset LDFLAGS
 export PATH="${PREFIX}/bin:${PATH}"
 
-${PYTHON} setup.py install || exit 1;
+${PYTHON} setup.py install
 nuc_data_make
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    cp build/pyne/lib/*.dylib $PREFIX/lib
-elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-    find . -name '*.so' | xargs chmod +x
-    cp build/pyne/lib/*.so $PREFIX/lib
-fi
 
