bbb61a0ecdafed99c0d6e0dbc3f7b98072cbf47e
Anthony Scopatz <scopatz@gmail.com>
2015-03-05 12:50:24 -0600

    no without iGeom

--- pytaps/build.sh
+++ pytaps/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-#python setup.py --iMesh-path=$PREFIX --iGeom-path=$PREFIX --iRel-path=$PREFIX \
-python setup.py --iMesh-path=$PREFIX --without-iGeom --iRel-path=$PREFIX \
-                install
+#python setup.py --iMesh-path=$PREFIX --iGeom-path=$PREFIX --iRel-path=$PREFIX install
+python setup.py --iMesh-path=$PREFIX --iRel-path=$PREFIX install
