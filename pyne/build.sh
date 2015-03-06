d593f5b42bff6771501e50b4f247dfa5d16c2531
Anthony Scopatz <scopatz@gmail.com>
2015-03-06 17:21:54 -0600

    copy so

--- pyne/build.sh
+++ pyne/build.sh
@@ -19,9 +19,12 @@ fi
 ${PYTHON} setup.py install
 nuc_data_make
 
+#
+# Annoying hack for copying dlls
+#
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  cp build/pyne/lib/*.dylib $PREFIX/lib
+  cp build/src/*.dylib $PREFIX/lib
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
   find . -name '*.so' | xargs chmod +x
-  cp build/pyne/lib/*.so $PREFIX/lib
+  cp build/src/*.so $PREFIX/lib
 fi
