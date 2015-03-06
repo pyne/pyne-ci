dda66d5a0006bcf9897c118d7d6f000107ae5045
Anthony Scopatz <scopatz@gmail.com>
2015-03-06 13:03:42 -0600

    test imporovements

--- conda_test.sh
+++ conda_test.sh
@@ -2,5 +2,15 @@
 set -x
 set -e
 source conda_env.sh
-ls 
-nosetests -w ./pyne-src/tests
+ls $BLD
+
+pyne_tar_dir="pyne-unichr"
+
+# Setup workdir
+if [ -d "${BLD}/work/${cycamore_tar_dir}" ]; then
+  export WORKDIR="${BLD}/work/${cycamore_tar_dir}"
+else  
+  export WORKDIR="${BLD}/work"
+fi
+
+nosetests -w "${WORKDIR}/tests"
