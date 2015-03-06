b6b61654d82228d444758724353a9c95f780a74b
Anthony Scopatz <scopatz@gmail.com>
2015-03-06 15:37:21 -0600

    try

--- pyne/build.sh
+++ pyne/build.sh
@@ -19,3 +19,9 @@ fi
 ${PYTHON} setup.py install
 nuc_data_make
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  cp build/pyne/lib/*.dylib $PREFIX/lib
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+  find . -name '*.so' | xargs chmod +x
+  cp build/pyne/lib/*.so $PREFIX/lib
+fi
