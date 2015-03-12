5747454e4d8843aa1bce9b728b156fc959947056
Anthony Scopatz <scopatz@gmail.com>
2015-03-12 18:08:28 -0500

    glibc removal

--- conda_fast_build.sh
+++ conda_fast_build.sh
@@ -6,14 +6,10 @@ set -e
 ./bin/conda-inst.sh
 source conda_env.sh
 
-if [[ "$(uname)" != "Linux" ]]; then
-  conda install gcc
-fi
-
 # get glibc version, minorly apologetic for this.
-export LDD_VER="$(ldd --version)"
-export GLIBC_MAJOR_VERSION=$(python -c "print('''${LDD_VER}'''.splitlines()[0].split()[-1].split('.')[0])")
-export GLIBC_MINOR_VERSION=$(python -c "print('''${LDD_VER}'''.splitlines()[0].split()[-1].split('.')[1])")
+#export LDD_VER="$(ldd --version)"
+#export GLIBC_MAJOR_VERSION=$(python -c "print('''${LDD_VER}'''.splitlines()[0].split()[-1].split('.')[0])")
+#export GLIBC_MINOR_VERSION=$(python -c "print('''${LDD_VER}'''.splitlines()[0].split()[-1].split('.')[1])")
 
 # install deps
 conda install nose jinja2 pytables hdf5 scipy cython cmake moab
@@ -23,9 +19,9 @@ if [[ "$MINICONDA_PYVER" == "2" ]]; then
   conda install pytaps
 fi
 
-if [ "14" -gt "$GLIBC_MINOR_VERSION" ]; then
-  conda install glibc
-fi
+#if [ "14" -gt "$GLIBC_MINOR_VERSION" ]; then
+#  conda install glibc
+#fi
 
 # build and install pyne conda package
 conda_build pyne
